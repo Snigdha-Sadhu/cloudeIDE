b1dfe50040ec256e24353356382124ad8c69539e
Snigdha-Sadhu <snigdhasadhu2005@gmail.com>
2026-01-31 07:45:07 +0530

    changing url

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API=axios.create({
-    baseURL:'http://localhost:8000/api',
+    baseURL:'https://cloudeide.onrender.com/api',
     headers:{
         'Content-Type':'application/json',
     },
