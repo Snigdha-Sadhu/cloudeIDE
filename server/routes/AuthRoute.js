39b7af1d9650455c7e90d12591bfdad3bd2e6148
Snigdha-Sadhu <snigdhasadhu2005@gmail.com>
2026-01-31 07:27:54 +0530

    changing file name

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router=express.Router();
-import { signup,login,getMe } from '../controllers/AuthController.js';
+import { signup,login,getMe } from '../controllers/authController.js';
 import {auth} from '../middleware/Auth.js';
 router.post('/signup',signup);
 router.post('/login',login);
